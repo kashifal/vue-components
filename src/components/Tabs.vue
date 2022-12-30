4acf30ca3dfdce4c14e929443723d857a2e84ff2
usama arslan <usama.arslan@softpyramid.com>
2022-12-30 15:59:53 +0500

    api

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,23 +1,30 @@
 <template>
   <div class=" mt-12 max-w-l ">
     <div class="tabs ml-2 bg-rose-400 inline-flex rounded overflow-hidden px-2 py-1 ">
-      <button v-for="(btn, index) in btns" :key="index" class="px-4 py-1 rounded  hover:bg-white/30 text-white ml-2" @click="setIndex(index)" :class="active_tab === index ? 'bg-white hover:bg-white text-rose-400 rounded transition-all' : ''">{{ btn }}</button>
+
+      <button v-for="(btn, index) in btns" :key="index" class="px-4 py-1 rounded  hover:bg-white/30 text-white ml-2" @click="setIndex(index, btn)" :class="active_tab === index ? 'bg-white hover:bg-white text-rose-400 rounded transition-all' : ''">{{ btn }}
+
+
+      </button>
     </div>
     <div class="tab-data mt-4 ml-4">
-      <p v-for="(data, index) in data"    :key="data.id">{{active_data === index ?  data.text : ''}}</p>
+<!--      <p v-for="(data, index) in data"    :key="data.id">{{active_data === index ?  data.text : ''}}</p>-->
+      <p v-for="country in c_data" :key="country.area">{{country.flag}} <span>{{country.capital[0]}}</span> <span>{{country.name.common}}</span> <span>{{country.population/1000000}}Millions</span></p>
 
     </div>
   </div>
 </template>
 
 <script>
-import {ref} from "vue";
+import {ref,onMounted} from "vue";
 const active_tab = ref(0);
 const active_data = ref(0);
+const route = ref('https://restcountries.com/v3.1/name/pakistan');
+const c_data = ref([]);
 export default {
 
   setup() {
-  const btns = [ 'one', 'two', 'three'];
+  const btns = [ 'pakistan', 'india', 'usa', 'afghanistan','iran','canada','japan'];
   const data = [
       {
     id:1,
@@ -32,17 +39,35 @@ export default {
   ];
 
 
-  function  setIndex(i){
+  function  setIndex(i,country){
     active_tab.value = i;
   active_data.value = i;
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+      .then((res) => {
+        return res.json()
+      }).then((data) => {
+    c_data.value = data;
+  })
+
+    console.log(c_data.value)
   }
 
+  onMounted(() => {
+    fetch(`https://restcountries.com/v3.1/name/pakistan`)
+        .then((res) => {
+          return res.json()
+        }).then((data) => {
+      c_data.value = data;
+    })
+    console.log(c_data.value)
+  })
     return {
       btns,
       data,
       setIndex,
       active_tab,
-      active_data
+      active_data,
+      c_data
     }
   }
 }
